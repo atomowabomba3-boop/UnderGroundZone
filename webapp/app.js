3f70504b1407d8de6c46d2c6deb6367489835662
atomowabomba3-boop <atomowabomba3@gmail.com>
2026-07-25 17:57:11 +0200

    Update app.js

--- webapp/app.js
+++ webapp/app.js
@@ -1,4 +1,45 @@
-let userId = 123456;
+let userId = null;
+
+
+
+function getTelegramUser(){
+
+    if(window.Telegram && Telegram.WebApp){
+
+        Telegram.WebApp.ready();
+
+
+        let user =
+        Telegram.WebApp.initDataUnsafe.user;
+
+
+        if(user){
+
+            userId = user.id;
+
+            console.log(
+                "Telegram user:",
+                userId
+            );
+
+        }
+
+    }
+
+
+    // test poza Telegramem
+
+    if(!userId){
+
+        userId = 123456;
+
+    }
+
+}
+
+
+
+getTelegramUser();
 
 
 const tickets = document.getElementById("tickets");
