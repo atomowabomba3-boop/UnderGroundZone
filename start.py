30bc097b908b92e2877d9c87730c9c0da995bf8f
atomowabomba3-boop <atomowabomba3@gmail.com>
2026-07-25 16:51:14 +0200

    Create start.py

---
+++ start.py
@@ -0,0 +1,29 @@
+import subprocess
+import time
+
+
+bot = subprocess.Popen(
+    [
+        "python",
+        "main.py"
+    ]
+)
+
+
+time.sleep(2)
+
+
+api = subprocess.Popen(
+    [
+        "uvicorn",
+        "api:app",
+        "--host",
+        "0.0.0.0",
+        "--port",
+        "8080"
+    ]
+)
+
+
+bot.wait()
+api.wait()
